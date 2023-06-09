474067d3e8f727155d4ff147c804a7e95bc353d0
Adsuarez <adsuarezq@gmail.com>
2023-06-09 15:30:37 -0500

    add object of images and url in constants for soldering service

--- src/js/constants.js
+++ src/js/constants.js
@@ -2,6 +2,7 @@ export const $ = (element) => document.querySelector(element);
 
 export const URLS_MULTIMEDIA = {
   painting: "../../multimedia/painting/",
+  soldering: "../../multimedia/soldering/",
 };
 
 export const multimedia = [
@@ -13,6 +14,13 @@ export const multimedia = [
       `${URLS_MULTIMEDIA.painting}pexels-kaboompics-com-6119.jpg`,
       `${URLS_MULTIMEDIA.painting}pintura-y-demarcacion-de-parqueaderos-y-bodegas-bogota_2c64860fda_3.jpg`,
       `${URLS_MULTIMEDIA.painting}pintura-y-demarcacion-de-parqueaderos-y-bodegas-bogota_39c54b257_3.jpg`,
+    ],
+  },
+  {
+    group: "soldering",
+    images: [
+      `${URLS_MULTIMEDIA.soldering}pexels-pixabay-73833.jpg`,
+      `${URLS_MULTIMEDIA.soldering}cual-es-la-lamina-mas-barata-para-techar.jpg`,
     ],
   },
 ];
