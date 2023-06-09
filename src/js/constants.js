2750c28b59e233c54702b06ad485481e3a50ff81
Adsuarez <adsuarezq@gmail.com>
2023-06-09 12:48:50 -0500

    create file to manage constants in a module

---
+++ src/js/constants.js
@@ -0,0 +1,18 @@
+export const $ = (element) => document.querySelector(element);
+
+export const URLS_MULTIMEDIA = {
+  painting: "../../multimedia/painting/",
+};
+
+export const multimedia = [
+  {
+    group: "painting",
+    images: [
+      `${URLS_MULTIMEDIA.painting}montacargas-despues-01.jpg`,
+      `${URLS_MULTIMEDIA.painting}montacargas-antes-01.jpg`,
+      `${URLS_MULTIMEDIA.painting}pexels-kaboompics-com-6119.jpg`,
+      `${URLS_MULTIMEDIA.painting}pintura-y-demarcacion-de-parqueaderos-y-bodegas-bogota_2c64860fda_3.jpg`,
+      `${URLS_MULTIMEDIA.painting}pintura-y-demarcacion-de-parqueaderos-y-bodegas-bogota_39c54b257_3.jpg`,
+    ],
+  },
+];
