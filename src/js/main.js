ca7610672d8b2537351fe08cdcaf36cd1f811b8b
Adsuarez <adsuarezq@gmail.com>
2023-06-09 16:34:48 -0500

    import designA values and add sliders events

--- src/js/main.js
+++ src/js/main.js
@@ -21,8 +21,16 @@ import {
   solderingMaxIndex,
 } from "./solderingValues.js";
 
+import {
+  nextSlideButtonDesignA,
+  prevSlideButtonDesignA,
+  servicesDesignAItem,
+  designAImages,
+  designAMaxIndex,
+} from "./design_aValues.js";
+
+/*PAINTING SERVICE*/
 let paintingIndex = 0;
-let solderingIndex = 0;
 
 prevSlideButtonPainting.addEventListener("click", () => {
   paintingIndex = prevIndex(paintingIndex);
@@ -36,6 +44,9 @@ nextSlideButtonPainting.addEventListener("click", () => {
   changeBackgroundImage(paintingImages[paintingIndex], servicesPaintingItem);
 });
 
+/*SOLDERING SERVICE */
+let solderingIndex = 0;
+
 prevSlideButtonSoldering.addEventListener("click", () => {
   solderingIndex = prevIndex(solderingIndex);
   solderingIndex = setImageIndex(solderingIndex, solderingMaxIndex);
@@ -46,4 +57,19 @@ nextSlideButtonSoldering.addEventListener("click", () => {
   solderingIndex = nextIndex(solderingIndex);
   solderingIndex = setImageIndex(solderingIndex, solderingMaxIndex);
   changeBackgroundImage(solderingImages[solderingIndex], servicesSolderingItem);
+});
+
+/*DESIGN A SERVICE */
+let designAIndex = 0;
+
+prevSlideButtonDesignA.addEventListener("click", () => {
+  designAIndex = prevIndex(designAIndex);
+  designAIndex = setImageIndex(designAIndex, designAMaxIndex);
+  changeBackgroundImage(designAImages[designAIndex], servicesDesignAItem);
+});
+
+nextSlideButtonDesignA.addEventListener("click", () => {
+  designAIndex = nextIndex(designAIndex);
+  designAIndex = setImageIndex(designAIndex, designAMaxIndex);
+  changeBackgroundImage(designAImages[designAIndex], servicesDesignAItem);
 });
