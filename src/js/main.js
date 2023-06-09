32c573456f700b8bc8bb05ee0755ba1a046e5cd6
Adsuarez <adsuarezq@gmail.com>
2023-06-09 12:50:53 -0500

    clean main file extracting in modules and let only the necesary code ...pending add functions to Soldering service

--- src/js/main.js
+++ src/js/main.js
@@ -1,52 +1,28 @@
-const URLS_MULTIMEDIA = {
-  painting: "../../multimedia/painting/",
-};
-const multimedia = [
-  {
-    group: "painting",
-    images: [
-      `${URLS_MULTIMEDIA.painting}montacargas-despues-01.jpg`,
-      `${URLS_MULTIMEDIA.painting}montacargas-antes-01.jpg`,
-      `${URLS_MULTIMEDIA.painting}pexels-kaboompics-com-6119.jpg`,
-      `${URLS_MULTIMEDIA.painting}pintura-y-demarcacion-de-parqueaderos-y-bodegas-bogota_2c64860fda_3.jpg`,
-      `${URLS_MULTIMEDIA.painting}pintura-y-demarcacion-de-parqueaderos-y-bodegas-bogota_39c54b257_3.jpg`,
-    ],
-  },
-];
+import {
+  changeBackgroundImage,
+  nextIndex,
+  prevIndex,
+  setImageIndex,
+} from "./helpers.js";
 
-const getArrayByGroup = (groupName) => {
-  const [groupObject] = multimedia.filter((item) => item.group === groupName);
-  return groupObject.images;
-};
+import {
+  nextSlideButtonPainting,
+  paintingImages,
+  paintingMaxIndex,
+  prevSlideButtonPainting,
+  servicesPaintingItem,
+} from "./paintingValues.js";
 
-let index = 0;
-const paintingImages = getArrayByGroup("painting");
-const paintingMaxIndex = paintingImages.length;
+export let paintinIndex = 0;
 
-const servicesPaintingItem = document.querySelector(".services-painting__item");
-const prevSlideButton = document.querySelector(".prevSlide");
-const nextSlideButton = document.querySelector(".nextSlide");
-
-prevSlideButton.addEventListener("click", () => {
-  index--;
-  manageImages();
+prevSlideButtonPainting.addEventListener("click", () => {
+  paintinIndex = prevIndex(paintinIndex);
+  paintinIndex = setImageIndex(paintinIndex, paintingMaxIndex);
+  changeBackgroundImage(paintingImages[paintinIndex], servicesPaintingItem);
 });
 
-nextSlideButton.addEventListener("click", () => {
-  index++;
-  manageImages();
+nextSlideButtonPainting.addEventListener("click", () => {
+  paintinIndex = nextIndex(paintinIndex);
+  paintinIndex = setImageIndex(paintinIndex, paintingMaxIndex);
+  changeBackgroundImage(paintingImages[paintinIndex], servicesPaintingItem);
 });
-
-const setImageIndex = () => {
-  if (index < 0) index = paintingMaxIndex - 1;
-  if (index === paintingMaxIndex) index = 0;
-};
-
-const changeBackgroundImage = (backgroundImage, element) => {
-  element.style.backgroundImage = `url(${backgroundImage})`;
-};
-
-const manageImages = () => {
-  setImageIndex();
-  changeBackgroundImage(paintingImages[index], servicesPaintingItem);
-};
