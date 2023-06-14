7870b85d9d25c7ae8df1e8134fe3990637075c0a
Adsuarez <adsuarezq@gmail.com>
2023-06-14 10:50:37 -0500

    update painting images according to names changed

--- src/js/constants.js
+++ src/js/constants.js
@@ -13,11 +13,11 @@ export const multimedia = [
   {
     group: "painting",
     images: [
-      `${URLS_MULTIMEDIA.painting}montacargas-despues-01.jpg`,
-      `${URLS_MULTIMEDIA.painting}montacargas-antes-01.jpg`,
-      `${URLS_MULTIMEDIA.painting}pexels-kaboompics-com-6119.jpg`,
-      `${URLS_MULTIMEDIA.painting}pintura-y-demarcacion-de-parqueaderos-y-bodegas-bogota_2c64860fda_3.jpg`,
-      `${URLS_MULTIMEDIA.painting}pintura-y-demarcacion-de-parqueaderos-y-bodegas-bogota_39c54b257_3.jpg`,
+      `${URLS_MULTIMEDIA.painting}1.jpg`,
+      `${URLS_MULTIMEDIA.painting}2.jpg`,
+      `${URLS_MULTIMEDIA.painting}3.jpg`,
+      `${URLS_MULTIMEDIA.painting}4.jpg`,
+      `${URLS_MULTIMEDIA.painting}5.jpg`,
     ],
   },
   {
